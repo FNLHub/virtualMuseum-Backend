bcb20000a53eca06738af61a4e42f88eecd0cf4e
Gerald Jumper <JumperGerald@users.noreply.github.com>
2019-03-24 03:03:27 -0700

    Upload File and Generate Empty Database Entry

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,30 @@
 import * as functions from 'firebase-functions';
+const admin = require('firebase-admin');
+const path = require('path');
 
- // Start writing Firebase Functions
- // https://firebase.google.com/docs/functions/typescript
 
- export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
- });
+admin.initializeApp();
+
+export const onFileUpload = functions.storage.object().onFinalize(object => {
+
+    const pathName = path.dirname(object.name);
+
+    // Only run decompression and DB update on files in the 'notProcessed' folder
+    if (pathName == 'notProcessed'){
+
+        // Cretes an empty object in our Firestore to be populated later from user uploaded JSON file
+        admin.firestore().collection('3dObjects').add({title: ""}).then((ref: any) => {
+            console.log(ref.id);
+            
+            // Unzip file into "3dObjects/{ref.id}"
+
+        })
+    }
+
+});
+
+export const onFileDelete = functions.storage.object().onDelete(event => {
+    console.log(event);
+    return;
+})
+
